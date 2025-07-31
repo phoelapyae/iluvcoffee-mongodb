5469d4c58d7c09d794ae2f7429dbf6abb38fe88a
phoelapyae <zinmyomaung17@gmail.com>
2025-08-01 00:37:03 +0700

    added mongo model

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,46 +1,62 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entities';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
-    private coffees: Coffee[] = [
-        {
-        id: 1,
-        name: 'Capiccano',
-        brand: 'Buddy Brew',
-        flavors: ['chocalla', 'vanilla']
-        }
-    ]
+    constructor(
+        @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>
+    ){}
 
     findAll() {
-        return this.coffees;
+        return this.coffeeModel.find().exec();
     }
 
-    findOne(id: string) {
-        const coffee = this.coffees.find(item => item.id === +id);
-        if (! coffee) {
-            throw new NotFoundException(`Coffee #{id} not found.`)
+    async findOne(id: string) {
+        if (!Types.ObjectId.isValid(id)) {
+            throw new NotFoundException(`Invalid ID format`);
         }
+
+        const coffee = await this.coffeeModel.findOne({ _id: new Types.ObjectId(id) }).exec();
+
+        if (!coffee) {
+            throw new NotFoundException(`Coffee #${id} not found.`)
+        }
+
         return coffee;
     }
 
-    create(createCoffeeDto: any) {
-        this.coffees.push(createCoffeeDto);
-        return createCoffeeDto;
+    create(createCoffeeDto: CreateCoffeeDto) {
+        const coffee = new this.coffeeModel((createCoffeeDto));
+        return coffee.save();
     }
 
-    update(id: string, updateCoffeeDto: any) {
-        const existingCoffee = this.findOne(id);
-
-        if (existingCoffee) {
-        // update the existing entity
+    async update(id: string, updateCoffeeDto: any) {
+        if (!Types.ObjectId.isValid(id)) {
+            throw new NotFoundException(`Invalid ID format`);
         }
+
+        const existingCoffee = await this.coffeeModel
+            .findOneAndUpdate(
+                { _id: new Types.ObjectId(id) },
+                { $set: updateCoffeeDto },
+                { new: true }
+            )
+            .exec();
+        
+        if (!existingCoffee) {
+            throw new NotFoundException(`Coffee #${id} not found.`);
+        }
+
+        return existingCoffee;
     }
 
-    remove(id: string) {
-        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
-        if (coffeeIndex >= 0) {
-        this.coffees.splice(coffeeIndex, 1);
+    async remove(id: string) {
+        if (!Types.ObjectId.isValid(id)) {
+            throw new NotFoundException(`Invalid ID format`);
         }
+        return this.coffeeModel.deleteOne({ _id: new Types.ObjectId(id) }).exec();
     }
 }
